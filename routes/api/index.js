0f005115c8a46f87b6b7739fb74aa158070297ba
Ann-Marie <anniedillier@gmail.com>
2020-10-15 00:20:21 -0700

    create post routs

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const userRoutes = require("./user-routes.js");
 
 router.use("/users", userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
